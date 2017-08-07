4557ef3780a4d49255e2eab8a129606bec2298f6
Yusuf <yusuf@eftsoftware.com>
2017-08-07 16:05:31 +0300

    Project init

---
+++ ColorFramework/ColorFramework.h
@@ -0,0 +1,19 @@
+//
+//  ColorFramework.h
+//  ColorFramework
+//
+//  Created by Yusuf Miletli on 8/7/17.
+//  Copyright © 2017 Miletli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorFramework.
+FOUNDATION_EXPORT double ColorFrameworkVersionNumber;
+
+//! Project version string for ColorFramework.
+FOUNDATION_EXPORT const unsigned char ColorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorFramework/PublicHeader.h>
+
+
---
+++ ColorFramework/GeneralColors.swift
@@ -0,0 +1,21 @@
+//
+//  GeneralColors.swift
+//  ColorFramework
+//
+//  Created by Yusuf Miletli on 8/7/17.
+//  Copyright © 2017 Miletli. All rights reserved.
+//
+
+import Foundation
+
+class GeneralColors {
+    
+    let shared:GeneralColors = GeneralColors()
+    
+    var blue:UIColor = UIColor.blue
+    
+    public func printColor() {
+        print("Color printing")
+    }
+    
+}
